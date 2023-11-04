36cd270731c895b0a7fc65944dcdfb91de1b6f36
iboscopilot <eaccount@ibos.io>
2023-11-04 20:46:50 +0600

    update

---
+++ src/SortableItem.jsx
@@ -0,0 +1,34 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import Item from "./Item";
+
+const SortableItem = (props) => {
+  const {
+    isDragging,
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id: props.id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition: transition || undefined,
+  };
+
+  console.log({id: props.id})
+
+  return (
+    <Item
+      ref={setNodeRef}
+      style={style}
+      withOpacity={isDragging}
+      {...props}
+      {...attributes}
+      {...listeners}
+    />
+  );
+};
+
+export default SortableItem;
